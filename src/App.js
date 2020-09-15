00a26cfc80d561b0667681100f35ff4fdde105bd
priyanka9515 <priyankadumala@gmail.com>
2020-09-15 12:34:49 +0530

    added all routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
-import List from "./Components/list";
-import Pop from "./Components/pop";
+// import List from "./Components/list";
+// import Pop from "./Components/pop";
 import { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./Components/Navbar";
+import ProductList from "./Components/ProductList";
+import Details from "./Components/Details";
+import Default from "./Components/Default";
+import Cart from "./Components/Cart";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      abc: 5,
-    };
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     abc: 5,
+  //   };
+  // }
   render() {
     return (
-      <div className="App">
-        {/* <h1>fcxv</h1>
-      <h2>bdb</h2> */}
-        <List />
-        <Pop />
-      </div>
+      // <div className="App">
+      //   {/* <h1>fcxv</h1>
+      // <h2>bdb</h2> */}
+      //   <List />
+      //   <Pop />
+      // </div>
+      <React.Fragment>
+        <Navbar />
+        <Switch>
+          <Route path="/" Component={ProductList} />
+
+          <Route path="/details" Component={Details} />
+          <Route path="/cart" Component={Cart} />
+
+          <Route Component={Default} />
+        </Switch>
+      </React.Fragment>
     );
   }
 }
---
+++ src/Components/Product.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Product extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h3>hello im product</h3>
+      </div>
+    );
+  }
+}
+
+export default Product;
