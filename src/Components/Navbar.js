6f5461901fee6fb859b600b9c8a4631ed0773954
priyanka9515 <priyankadumala@gmail.com>
2020-09-15 13:43:29 +0530

    added navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,12 +1,31 @@
 import React, { Component } from "react";
-
+import { Link } from "react-router-dom";
+import logo from "../logo.svg";
 class Navbar extends Component {
   state = {};
   render() {
     return (
-      <div>
-        <h3>hello from navbar</h3>
-      </div>
+      <nav
+        className="navbar navbar-expand-sm navbar-dark px-sm-5"
+        style={{ backgroundColor: "blue", height: "8" }}
+      >
+        <Link to="/">
+          <img src={logo} alt="store" />
+        </Link>
+        <ul className="navbar-nav.align-items">
+          <li className="nav-item ml-5">
+            <Link to="/" className="nav-link">
+              products
+            </Link>
+          </li>
+        </ul>
+        <Link to="/cart" className="ml-auto">
+          <button>
+            <i className="fas fa-cart-plus" />
+            my cart
+          </button>
+        </Link>
+      </nav>
     );
   }
 }
